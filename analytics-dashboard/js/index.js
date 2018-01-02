367c32ead08aa955a0af12b2087d90f70b10586a
jaythomasv29 <34979065+jaythomasv29@users.noreply.github.com>
2018-01-01 21:57:46 -0800

    Add files via upload

---
+++ analytics-dashboard/js/index.js
@@ -0,0 +1,14 @@
+/**
+ * Built with the jQuery plugin
+ *
+ **/
+$('.percentage').easyPieChart({
+  animate: 1000,
+  lineWidth: 4,
+  onStep: function(value) {
+    this.$el.find('span').text(Math.round(value));
+  },
+  onStop: function(value, to) {
+    this.$el.find('span').text(Math.round(to));
+  }
+});
